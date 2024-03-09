9d66f744824ebc05c81dc09f8aab00f15b240a35
jeosafa eller <jeosafaeller@gmail.com>
2024-03-09 07:34:21 -0300

    Controller Usuarios criado

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,87 @@
+﻿using mf_api_web_services_fuel_manager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_api_web_services_fuel_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.Usuarios.ToListAsync();
+            return Ok(model);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(Usuario model)
+        {
+           
+
+            _context.Usuarios.Add(model);
+            await _context.SaveChangesAsync();
+
+            //metodo 201 nome do metodo , rota e modelo
+
+            return CreatedAtAction("GetById", new { id = model.Id }, model);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Usuarios
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (model == null) return NotFound();
+
+            return Ok(model);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, Usuario model)
+        {
+            //validação
+            if (id != model.Id) return BadRequest();
+            //colocar o as notraking para funcionar
+            var modeloDB = await _context.Usuarios.AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (modeloDB == null) return NotFound();
+
+            _context.Usuarios.Update(model);
+            await _context.SaveChangesAsync();
+
+            //ja passou a atualização e não espera receber nada 204
+            return NoContent();
+
+        }
+
+        //HttpDelete
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var model = await _context.Usuarios.FindAsync(id);
+
+            if (model == null) return NotFound();
+
+            _context.Usuarios.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return Ok(model);
+
+        }
+
+        
+
+
+
+
+    }
+}
